6007baac4e44ea42bf91206f3ffc857af58c1691
cotton <chinoknct@gmail.com>
2020-03-03 14:50:01 +0900

    [add]宣言編集および応援メッセージ取得の機能追加
    declaration.jsにおいて宣言編集および応援メッセージ取得の機能を追加しました。

--- api/declaration.js
+++ api/declaration.js
@@ -14,13 +14,14 @@ app.post('/declarations', function(req, res, next) {
         hasp = req.body.hasp,
         // 仮置き storeの値取得方法を確認してから修正
         // declarer = this.$store.state.user._id,
-        tag = req.body.tag
+        tag = req.body.tag;
 
     let newDeclaration = new Declaration({
         index: User.find({}).count() - 1,
         name: declarationTitle,
         hasp: hasp,
-        supporter: '',
+        declarer: {},
+        supporter: [],
         tag: tag
     });
 
@@ -34,7 +35,7 @@ app.post('/declarations', function(req, res, next) {
 
 //宣言詳細取得
 app.get('/declarations/.+', function(req, res, next) {
-    Declaration.findOne({ _id: this.$store.state.user._id}, function(err, result) {
+    Declaration.findOne({ _id: '宣言の_id'}, function(err, result) {
         if(err) {
             console.log(err);
         }
@@ -44,28 +45,70 @@ app.get('/declarations/.+', function(req, res, next) {
 
 //宣言編集
 app.put('/declarations/.+', function(req, res, next) {
-    Declaration.findOne({ _id: this.$store.state.user._id}, function(err, result) {
-        if(err) {
-            console.log(err);
-        }
-    });
-})
+    let declarationTitle = req.body.declarationTitle,
+        hasp = req.body.hasp,
+        // 仮置き storeの値取得方法を確認してから修正
+        // declarer = this.$store.state.user._id,
+        tag = req.body.tag;
 
-//編集完了
-app.put('/declarations/.+', function(req, res, next) {
-    
-})
+    let updateDeclaration = new Declaration({
+        index: User.find({}).count() - 1,
+        name: declarationTitle,
+        hasp: hasp,
+        declarer: {},
+        supporter: [],
+        tag: tag
+    });
+
+    Declaration.update({ _id: '宣言の_id' }, updateDeclaration, {upsert: true}, function(err) {
+            if(err) {
+                console.log(err);
+            }
+            res.redirect('/');
+        }
+    );
+});
 
 //応援メッセージ取得
 app.post('/declarations/.+/support/message', function(req, res, next) {
-    
-})
+    Declaration.findOne({ _id: this.$store.state.user._id})
+        .populate('supporters')
+        .exec( function(err, result) {
+            if(err) {
+                console.log(err);
+            }
+            res.send(result.supporters);
+        });
+});
 
 //応援ボタン押下
 app.post('/declarations/.+/support', function(req, res, next) {
 
-})
+});
 
+//サンプルデータ取得用
+app.get('/test', function(req, res, next) {
+    let declarationTitle = 'サンプルプロジェクト',
+        hasp = 1000,
+        declarer = 'コットン',
+        tag = 'nuxt'
+
+    let newDeclaration = new Declaration({
+        index: User.find({}).count() - 1,
+        name: declarationTitle,
+        hasp: hasp,
+        declarer: declarer,
+        supporter: [],
+        tag: tag
+    });
+
+    newDeclaration.save(function(err) {
+        if(err) {
+            console.log(err);
+        }
+        res.send();
+    });
+})
 
 module.exports = {
     path: "/api/",
