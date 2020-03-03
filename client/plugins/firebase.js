ba8651798f99cb4b8342a1168735a5e9f9d33ca7
cotton <chinoknct@gmail.com>
2020-03-04 01:30:40 +0900

    [add]firebase.jsの追加
    firebase.jsの追加を行いました。

---
+++ client/plugins/firebase.js
@@ -0,0 +1,15 @@
+// import firebase from 'firebase'
+
+// if(!firebase.apps.length) {
+//     firebase.initializeApp({
+//         apiKey: "AIzaSyCHPxHmPWoF38tWOz6MTme9MxLdtNC-NNc",
+//         authDomain: "seagull-548c5.firebaseapp.com",
+//         databaseURL: "https://seagull-548c5.firebaseio.com",
+//         projectId: "seagull-548c5",
+//         storageBucket: "seagull-548c5.appspot.com",
+//         messagingSenderId: "202918288762",
+//         appId: "1:202918288762:web:d4cb0c6a044b29d084cac3"
+//     })
+// }
+
+// export default firebase
