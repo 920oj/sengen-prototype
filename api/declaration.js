45564ed06daf9ab9540dc168c1767cad7b844879
cotton <chinoknct@gmail.com>
2020-03-03 12:45:22 +0900

    [add]宣言詳細取得機能追加
    declaration.jsにて宣言の詳細を取得する機能を追加しました。

--- api/declaration.js
+++ api/declaration.js
@@ -13,12 +13,11 @@ app.post('/declarations', function(req, res, next) {
     let declarationTitle = req.body.declarationTitle,
         hasp = req.body.hasp,
         // 仮置き storeの値取得方法を確認してから修正
-        // declarer = this.$store.state.user.uid,
+        // declarer = this.$store.state.user._id,
         tag = req.body.tag
 
     let newDeclaration = new Declaration({
-        //仮置き 固有の番号取得方法を用意してから
-        // index: '',
+        index: User.find({}).count() - 1,
         name: declarationTitle,
         hasp: hasp,
         supporter: '',
@@ -30,17 +29,26 @@ app.post('/declarations', function(req, res, next) {
             console.log(err);
         }
         res.redirect('/');
-    })
-})
+    });
+});
 
 //宣言詳細取得
 app.get('/declarations/.+', function(req, res, next) {
-
-})
+    Declaration.findOne({ _id: this.$store.state.user._id}, function(err, result) {
+        if(err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
 
 //宣言編集
 app.put('/declarations/.+', function(req, res, next) {
-    
+    Declaration.findOne({ _id: this.$store.state.user._id}, function(err, result) {
+        if(err) {
+            console.log(err);
+        }
+    });
 })
 
 //編集完了
