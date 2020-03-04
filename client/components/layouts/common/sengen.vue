1f81c31f55e9289f0bf5af0fd5279e9dc4ef92c2
Junya Oiwa <0920oiwa@gmail.com>
2020-03-05 01:45:14 +0900

    [add]宣言一覧用コンポーネントを作成

---
+++ client/components/layouts/common/sengen.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="sengen">
+    <div class="sengen-wrapper">
+      <div class="sengen-category">
+        {{category}}
+      </div>
+      <div class="sengen-img">
+        <!-- あとで:src="img"にする -->
+        <img src="~/assets/img/png/ogp.png" alt="サムネイル">
+      </div>
+      <div class="sengen-description">
+        <div class="sengen-desc">
+          <p>宣言pt: {{point}}</p>
+          <p>応援者数: {{supporter}}人</p>
+          <p>残り: {{expires}}日</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'category',
+    'title',
+    'img',
+    'point',
+    'supporter',
+    'expires',
+  ]
+}
+</script>
+
+<style scoped>
+
+</style>
