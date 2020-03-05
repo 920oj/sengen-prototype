8de96449854d34845597a86116b79da2d18608c2
Junya Oiwa <0920oiwa@gmail.com>
2020-03-06 00:27:00 +0900

    [add]ページネーションのスタイルを切り替えるメソッドを用意

--- client/components/ui/nav/paginationNav.vue
+++ client/components/ui/nav/paginationNav.vue
@@ -1,15 +1,43 @@
 <template>
   <div class="pagination">
+    <div class="pagination-container">
 
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      'right_only': false,
+      'common': false,
+      'left_only': false,
+    }
+  },
   props: [
     'max',
     'current'
-  ]
+  ],
+  computed: {
+    changeStyle: function() {
+      if(this.current == 1) {
+        this.right_only = true;
+        this.common = false;
+        this.left_onlu = false;
+      }
+      else if (this.current == this.max) {
+        this.right_only = true;
+        this.common = false;
+        this.left_onlu = false;
+      }
+      else {
+        this.right_only = false;
+        this.common = false;
+        this.left_onlu = true;
+      }
+    }
+  }
 }
 </script>
 
