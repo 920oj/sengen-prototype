1d0528b3786fcf8a23e894400ee9cbcbe44c8fa3
cotton <chinoknct@gmail.com>
2020-03-08 12:38:52 +0900

    [update]検索機能の修正
    挙動がおかしくなるんですが一応検索はできるように修正しました。

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const bodyParser = require('body-parser');
 const database = require('./database.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 const User = database.User;
+const Declaration = database.Declaration;
 
 let declaration = require('./declaration');
 let auth = require('./auth');
@@ -21,24 +23,57 @@ app.use('/', point);
 //検索
 app.post('/search/:keyword', function(req, res, next) {
     let keyword = req.params.keyword;
+    
+    Declaration.find({name: new RegExp(`${keyword}`)}, function(err, result) {
+        let declarationLength = Object.keys(result).length;
+        postDeclarations(result, declarationLength);
+        
+        function postDeclarations (result, declarationLength) {
+            let start = 0,
+            end = 10,
+            countPages = 1,
+            pageDatas = new Map,
+            tenCountLength = declarationLength,
+            sendData = [];
+            
+            while(tenCountLength > 10) {
+                pageDatas.set(countPages, result.slice(start, end));
+                start += 10;
+                end += 10;
+                tenCountLength -= 10;
+                countPages++;
+            }
+            end = declarationLength;
+            pageDatas.set(countPages, result.slice(start, end));
+            
+            while(countPages > 0) {
+                let setData = pageDatas.get(countPages);
+                sendData[countPages - 1] = setData
+                countPages--;
+            }
+            res.send(sendData);
+        }
 
-    // Declaration.find({
-    //     name: `/${keyword}/`,
-    //     overview: `/${keyword}/`,
-    //     detail: `/${keyword}/` },
-    //     function(err, result) {
-    //         //条件が重複した回数をカウント（DISTINCT）
-    //         let resultLength = Object.keys(result).length;
-    //         while(resultLength > 0) {
-    //             resultLegth--;
-    //         }
-    //     }
-    // );
-    console.log(keyword);
+        // if(declarationLength < 100) {
+        //     Declaration.find({ index: { $gte: 0, $lte: declarationLength}})
+        //         .populate('declarer')
+        //         .exec( function(err, result) {
+        //             if(err) {
+        //                 console.log(err);
+        //             }
+        //         });
+        //     } 
+    })
+
+            //条件が重複した回数をカウント（DISTINCT）
+            // let resultLength = Object.keys(result).length;
+            // while(resultLength > 0) {
+            //     resultLegth--;
+            // }
+
     // Declaration.find({ name: `/${keyword}/`}, function(err, result) {
     //     console.log(result);
     // })
-        res.send();
 });
 
 //カテゴリーページ
